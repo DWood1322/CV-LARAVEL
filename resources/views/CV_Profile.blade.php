d79a890f09e1cfd7606838e8e0c6dbf45754a830
DWood1322 <echo.arlandanaufal1322@gmail.com>
2023-01-19 09:02:52 +0700

    Menambahkan Code Laravel CV

---
+++ resources/views/CV_Profile.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CV</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <table>
+        <tr>
+            <th>Web Dev</th>
+            <th></th>
+            <td></td>
+            <td>arlandanaufal1322@gmail.com</td>
+        </tr>
+        
+        <tr>
+            <td></td>
+            <div> <td class="nama"><b>Arlanda</b></td></div>
+            <td></td>
+       
+        <td><img src="https://w7.pngwing.com/pngs/842/713/png-transparent-user-profile-computer-icons-others-miscellaneous-face-monochrome.png" alt="Foto"></td>
+        </tr>
+    
+     <tr>
+       <td></td>
+       <td></td>
+       <td></td>
+        
+    </tr>
+    </table>
+    
+    <table class="table2">
+        <tr>
+            <th colspan="2">Profile</th>
+            <th>        </th>
+            <th>         </th>
+            
+            <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eveniet necessitatibus voluptatem accusantium consequatur odit, quas dicta eaque laborum vero ex cumque, iure maxime exercitationem provident corrupti velit impedit aspernatur.</td>
+        </tr>
+        
+        <tr>
+            <td></td>
+            <td></td>
+            <td>SKIL</td>
+            <td></td>
+       
+       
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+            <ul>
+                <li>HTML</li>
+            </ul>
+        </tr>
+    
+     <tr>
+       <td></td>
+       <td></td>
+       <td></td>
+        
+    </tr>
+    </table>
+    <table>
+
+    </table>
+</body>
+</html>
